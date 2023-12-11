0aa99eb318386c9edaf9e1e9bbdbaf2417b3e4ca
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2023-12-12 04:28:00 +0600

    img update

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -16,13 +16,14 @@ const Project = () => {
       <h1 className="mt-32 mb-20 font-bold text-4xl text-center">
         My Projects
       </h1>
-      <section className="flex flex-wrap justify-center">
+      <section className="flex flex-wrap mt-10 mb-10 gap-20 justify-center">
         {projectsData.map((project, index) => (
           <div key={index} className="card w-full bg-base-200 shadow-xl m-4">
             <div className="card-body">
               <div>
-                <h2 className="card-title">{project.title}</h2>
+                <h2 className="card-title text-5xl mt-2 mb-5">{project.title}</h2>
               </div>
+            <img src={project.img1} alt="" />
               <h2 className="card-title mt-5">
                 <span className="text-blue-500">Technology</span>:{" "}
                 {project.Technology}
